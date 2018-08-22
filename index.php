f1735c13ca985a86c18b7fbfae096285808680b0
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-22 10:30:42 +0200

    Correction avec la classe

--- index.php
+++ index.php
@@ -1,50 +1,29 @@
-<?php
-$lastNameErr = $firstNameErr = '';
-$lastName = $firstName = '';
 
-if(empty($_POST['lastName'])){
-  $lastNameErr = 'Ce champs est vide';
-} elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $lastName)){
-  $lastNameErr = 'Votre saisie possède des caractères incorrects';
-}
-
-if(empty($_POST['firstName'])){
-  $firstNameErr =  'Ce champs est vide';
-} elseif (preg_match('/^[a-zA-Z\- éèêç]*$/', $firstName)){
-  $firstNameErr = 'Votre saisie possède des caractères incorrects';
-}
-?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="style.Css" />
     <title>Exercice 6</title>
   </head>
-  <body><!--Formulaire de qualité-->
-    <form class="form" action="#" method="post"><!--Création formulaire-->
-      <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" /><span class="error">* <?php echo $lastNameErr;?></span><br><br>
-      <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" /><span class="error">* <?php echo $firstNameErr;?></span><br><br>
-      <select class="choice" name="choice">
-        <option value="monsieur">Monsieur</option>
-        <option value="madame">Madame</option>
-        <option value="non_binaire">Non-Binaire</option>
-      </select><br><br>
-      <input type="submit" name="submit" />
-    </form>
+  <body><!--Condition pour afficher les informations dans une phrase complète si toutes les informations sont renseignées-->
     <?php
-    if((isset($_POST['lastName']) && !empty($_POST['lastName']) && preg_match('/^[a-zA-Z\- éèêç]*$/', $_POST['lastName'])) && (isset($_POST['firstName']) && !empty($_POST['firstName']) && preg_match('/^[a-zA-Z\- éèêç]*$/',
-    $_POST['firstName'])) && (isset($_POST['choice']) && !empty($_POST['choice']))){
-      echo 'Nom: ' . $_POST['lastName'];
-      ?>
-      <br />
-      <?php
-      echo 'Prénom: ' . $_POST['firstName'];
-      ?>
-      <br />
-      <?php
-      echo 'Genre: ' . $_POST['choice'];
-    }
+      if(!empty($_POST['lastName']) && !empty($_POST['firstName']) && !empty($_POST['civility'])){
+        echo 'Je m\'appelle ' . $_POST['firstName'] . ' ' . $_POST['lastName'] . ' et je suis ' . $_POST['civility'];
+      } else {
+        ?>
+        <form action="#" method="POST"><!--Création formulaire qui ne s'affiche qu'au début et lorsque toutes les informations ne sont pas remplies-->
+          <label for="lastName">Votre nom: </label><input type="text" name="lastName" id="lastName" placeholder="nom" /><!--Champs nom de famille-->
+          <label for="firstName">Votre prénom: </label><input type="text" name="firstName" id="firstName" placeholder="prénom" /><!--Champs prénom-->
+          <select name="civility" id="civility"><!--Champs de sélection du sexe-->
+            <option selected disabled>Sélectionner votre sexe</option>
+            <option value="un homme">Masculin</option><!--Selection masculin-->
+            <option value="une femme">Féminin</option><!--Selection féminin-->
+            <option value="non_binaire">Non Binaire</option><!--Selection non-binaire-->
+          </select>
+          <input type="submit" name="submit" value="Soumettre" />
+        </form>
+        <?php
+      }
     ?>
   </body>
 </html>
